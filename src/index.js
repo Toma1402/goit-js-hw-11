d90b0de25b37697fef22113a731940e73919c4fe
Toma1402 <toma.k1402@gmail.com>
2022-11-27 17:45:33 +0000

    init

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 
 const formRef = document.querySelector('.search-form');
 const galleryRef = document.querySelector('.gallery');
+const loadRef = document.querySelector('.load-more');
+
 const API_KEY = '31598186-1712abd3d6ab8b33b97a57686';
-
-formRef.addEventListener('submit', onSubmit);
-
-function onSubmit(evt) {
-  evt.preventDefault();
-  let searchedImage = formRef.firstElementChild.value;
-  if (searchedImage) {
-    fetchImages((inputText = searchedImage))
-      .then(resp => resp.data.hits)
-      .then(data => {
-        console.log(data);
-        return createMarkup(data);
-        if (data.length === 0) {
-          console.log(1);
-        }
-      });
-  }
-  console.log(data);
-}
+const itemQuantity = 40;
+let groupNumber = 1;
 
 async function fetchImages() {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api?key=${API_KEY}&q=${inputText}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api?key=${API_KEY}&q=${inputText}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${itemQuantity}&page=${groupNumber}`
     );
-    return response;
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 }
+
+formRef.addEventListener('submit', onSubmit);
+function onSubmit(evt) {
+  evt.preventDefault();
+  let searchedImage = formRef.firstElementChild.value.trim();
+  clearHTML();
+
+  fetchImages((inputText = searchedImage)).then(resp => {
+    console.log(resp.hits);
+    if (resp.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadRef.classList.remove('is-visible');
+    }
+    Notify.success(`Hooray! We found ${resp.totalHits} images.`);
+    loadRef.classList.add('is-visible');
+    return createMarkup(resp.hits);
+  });
+}
+
 function createMarkup(arr) {
   const markup = arr
     .map(
@@ -55,8 +62,22 @@ function createMarkup(arr) {
     </div>`
     )
     .join('');
-  galleryRef.innerHTML = markup;
+  galleryRef.insertAdjacentHTML('beforeend', markup);
 }
+
+loadRef.addEventListener('click', () => {
+  groupNumber += 1;
+  fetchImages().then(resp => {
+    const totalImages = Math.ceil(resp.totalHits / itemQuantity);
+    if (groupNumber > totalImages) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    } else {
+      createMarkup(resp.hits);
+    }
+  });
+});
+
 function clearHTML() {
   galleryRef.innerHTML = '';
+  groupNumber = 1;
 }
